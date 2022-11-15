11de6a78eabc1345d8d1074912d27095c6152915
amapic <amaury.pichat@gmail.com>
2022-11-15 10:48:31 +0100

    Initial commit

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Rr() {
+  return "RR";
+}
+
+export default () => (
+  <>
+    <Rr />
+    <div>Hello World!</div>
+  </>
+);
---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+//import { Rr } from "./index";
+//import { shallow, mount, render } from "enzyme";
+it("works", () => {
+  expect(2).toEqual(2);
+});
+
+it("doesn't work", () => {
+  expect(2).toEqual(2);
+});
+
+//const wrapper = shallow(<Rr />);
